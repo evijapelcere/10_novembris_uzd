cb0cbea81b6257c8e40e4ee3bdd4440a92269668
evijapelcere <evijapelcere>
2024-11-27 21:28:16 +0200

    Paveicu uzdevumu

--- uzd_01_1.py
+++ uzd_01_1.py
@@ -1,3 +1,9 @@
-# Uzdevums: 01-1: Izveidot programmu, kas prasa lietotâjam ievadît skaitli n, tad programma aprçíina n+nn+nnn. 
-Rezultâts tiek parâdîts konsolç.
+# Uzdevums: 01-1: Izveidot programmu, kas prasa lietotâjam ievadīt skaitli n, tad programma aprēķina n+nn+nnn. 
+# Rezultāts tiek parādīts konsolç.
 
+n= int(input("Lūdzu, ievadi skaitli n: "))
+
+Iznakums= (int (n+n*n+n*n*n))
+
+
+print ("Iznākums =", Iznakums)
