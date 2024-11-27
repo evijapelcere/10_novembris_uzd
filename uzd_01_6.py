3770a639a0be3ed717b10812d6d4dee5c70a7870
evijapelcere <evijapelcere>
2024-11-28 00:00:43 +0200

    Paveicu uzdevumu

--- uzd_01_6.py
+++ uzd_01_6.py
@@ -1,6 +1,14 @@
-# Kâda valsts nolçma pâriet uz jaunu naudas sistçmu. Vecajâ sistçmâ bija trîs naudas vienîbas: dâlderis, grasis, santîms. Naudas vçrtîbas norâdîtas zemâk.
-# 1 dâlderis = 37 graði
+# Kâda valsts nolēma pâriet uz jaunu naudas sistēmu. Vecajâ sistēmā bija trîs naudas vienîbas: dâlderis, grasis, santîms. Naudas vērtîbas norâdîtas zemâk.
+# 1 dâlderis = 37 graši
 # 1 grasis = 162 santîmi
-# Jaunajâ naudas sistçmâ paliek tikai santîmi un dâlderi. Santîms saglabâ savu vçrtîbu, bet 1 dâlderis bûs vienâds ar 100 santîmiem. 
-Izveidot programmu, kas pârveidotu vecâs sistçmas naudu uz jaunu. Lietotâjam prasa ievadît vecâs sistçmas dâlderus, graðus un santîmus. Tiek aprçíinâts jaunâs sistçmas dâlderu un graðu daudzums. Rezultâts tiek parâdîts konsolç.
+# Jaunajâ naudas sistçmâ paliek tikai santîmi un dâlderi. Santîms saglabâ savu vērtîbu, bet 1 dâlderis bûs vienâds ar 100 santîmiem. 
+#Izveidot programmu, kas pârveidotu vecâs sistēmas naudu uz jaunu. Lietotâjam prasa ievadît vecâs sistēmas dâlderus, grašus un santîmus. Tiek aprķēinâts jaunâs sistēmas dâlderu un grašu daudzums. Rezultâts tiek parâdîts konsolē.
+dalderi=int(input("Lūdzu, ievadiet dālderu skaitu: "))
+grasi=int(input("Lūdzu, ievadiet grašu skaitu: "))
+santims=int(input("Lūdzu, ievadiet santīmu skaitu: "))
+grasi1=int(dalderi*37)
+santims1=int(grasi1*162)
+dalderi1=int(grasi1*162//100)
+print(f"Dālderu skaits ir {dalderi1} un santīmu skaits ir {santims1}")
+
 
