526ee84210b8ae8b094da41f652ca2a6eaa40eb5
evijapelcere <evijapelcere>
2024-11-27 22:12:43 +0200

    Paveicu uzdevumu

--- uzd_01_3.py
+++ uzd_01_3.py
@@ -1,2 +1,8 @@
 # Izveidot programmu, kura prasa lietotâjam sekunþu skaitu. Sekundes tiek pârveidotas par “x hours y minutes z seconds” tipa tekstu. Rezultâts tiek parâdîts konsolç.
+import math
+sekundes = float(input("Lūdzu, ievadi sekunžu skaitu: "))
+x= float(sekundes/3600)
+y= float(sekundes/60)
+z=sekundes
+print (f"Sekunžu skaits ir {x} stundas, {y} minūtes un {z} sekundes")
 
