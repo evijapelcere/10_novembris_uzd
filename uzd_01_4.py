a64bc5dc20dfab65366a56588855226d7673b61a
evijapelcere <evijapelcere>
2024-11-27 23:20:36 +0200

    Paveicu uzdevumu

--- uzd_01_4.py
+++ uzd_01_4.py
@@ -2,3 +2,8 @@
 
 sk1=float(input('Ievadi pirmo skaitli:'))
 sk2=float(input('Ievadi otro skaitli'))
+summa=int(sk1+sk2)
+starpiba=int(sk1-sk2)
+reizinajums=int(sk1*sk2)
+dalijums=int(sk1//sk2)
+print(f"Pirmā skaitļa un otrā skaitļa summa ir {summa}, starpība ir {starpiba}, reizinājums ir {reizinajums}, un dalījums ir {dalijums}. ")
