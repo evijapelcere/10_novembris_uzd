b1722f22157f2865acbc6925a5562d8a113564bb
evijapelcere <evijapelcere>
2024-11-27 23:29:58 +0200

    Paveicu uzdevumu

--- uzd_01_5.py
+++ uzd_01_5.py
@@ -1,4 +1,12 @@
 # Zemniekam ir govis, cûkas un vistas. Govîm un cûkâm ir pa 4 kâjâm, vistâm – 2. 
-Izveidot programmu, kas prasa lietotâjam ievadît cûku, govju un vistu skaitu. 
-Tiek aprçíinâts kopçjais kâju daudzums. Rezultâts tiek izvadîts konsolç.
+#Izveidot programmu, kas prasa lietotâjam ievadît cûku, govju un vistu skaitu. 
+#Tiek aprçíinâts kopçjais kâju daudzums. Rezultâts tiek izvadîts konsolç.
 
+cukas= int(input("Lūdzu, ievadiet cūku skaitu:"))
+govis= int(input("Lūdzu, ievadiet govju skaitu:"))
+vistas=int(input("Lūdzu, ievadiet vistu skaitu:"))
+skaits1=int(cukas*4)
+skaits2=int(govis*4)
+skaits3=int(vistas*2)
+kopskaits= int(skaits1+skaits2+skaits3)
+print (f"Kopējais, cūku, govju un vistu kāju skaits ir {kopskaits}.")
